abaa63fe40d0797cd226686d0879e85e2e3c4199
张耀文 <1650860607@qq.com>
2020-05-08 16:17:29 +0800

    根据代码规范整理代码

--- 01DelegateConsole/Program.cs
+++ 01DelegateConsole/Program.cs
@@ -58,15 +58,12 @@ namespace DelegateConsole
             //AsyncCallback参数传入回调方法(委托)
             //object参数传入回调State参数，回调方法中使用IAsyncResult参数类型的AsyncState属性获取
             //后台线程
-            IAsyncResult asyncResult = delInStrInvoke.BeginInvoke
-            (
-                "参数",
+            IAsyncResult asyncResult = delInStrInvoke.BeginInvoke("参数",
                 (p) =>
                 {
                     Console.WriteLine(p.AsyncState);
                 },
-                "回调State参数"
-            );
+                "回调State参数");
 
             while (!asyncResult.IsCompleted) { };//IsCompleted判断操作是否完成
             asyncResult.AsyncWaitHandle.WaitOne();//阻塞等待操作完成
