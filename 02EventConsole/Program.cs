27f3bc9d3a08ece8135f8986124ac6cfa824f0d5
张耀文 <1650860607@qq.com>
2020-10-19 14:25:09 +0800

    02Event

---
+++ 02EventConsole/Program.cs
@@ -0,0 +1,49 @@
+﻿using Library;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 事件初始化
+            MyEvent.InitEvent();
+            #endregion
+
+            #region 事件调用
+            #region 同步调用
+            MyEvent.ExecuteEvent();
+            #endregion
+
+            #region 异步调用
+            MyEvent.ExecuteEventAsync();
+            #endregion
+            #endregion
+
+            #region 事件订阅/取消订阅
+            Action action1 = () => { Console.WriteLine("事件1"); };
+            Action action2 = () => { Console.WriteLine("事件2"); };
+
+            //订阅
+            MyEvent.actionEvent += action1;
+            MyEvent.actionEvent += action2;
+
+            //取消订阅
+            MyEvent.actionEvent -= action1;
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Library/MyEvent.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class MyEvent
+    {
+        /// <summary>
+        /// Action类型事件
+        /// </summary>
+        public static event Action actionEvent;
+
+        /// <summary>
+        /// 初始化事件值
+        /// </summary>
+        public static void InitEvent()
+        {
+            actionEvent = () => { Console.WriteLine("事件初始化"); };
+        }
+
+        /// <summary>
+        /// 事件同步调用
+        /// </summary>
+        public static void ExecuteEvent()
+        {
+            actionEvent.Invoke();
+            actionEvent();//语法糖
+        }
+
+        /// <summary>
+        /// 事件异步调用
+        /// </summary>
+        public static void ExecuteEventAsync()
+        {
+            //AsyncCallback参数传入回调方法(委托)
+            //object参数传入回调State参数，回调方法中使用IAsyncResult参数类型的AsyncState属性获取
+            //后台线程
+            IAsyncResult asyncResult = actionEvent.BeginInvoke(
+                (p) =>
+                {
+                    Console.WriteLine(p.AsyncState);
+                },
+                "回调State参数");
+
+            while (!asyncResult.IsCompleted) { };//IsCompleted判断操作是否完成
+            asyncResult.AsyncWaitHandle.WaitOne();//阻塞等待操作完成
+            actionEvent.EndInvoke(asyncResult);//阻塞等待操作完成，获取返回类型
+        }
+    }
+}
