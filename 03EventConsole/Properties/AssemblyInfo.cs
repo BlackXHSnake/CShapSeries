14e488e36587e776b83cab9d05dc84712f7b3e8a
张耀文 <1650860607@qq.com>
2020-10-19 15:42:28 +0800

    修改项目命名

--- 03EventConsole/Properties/AssemblyInfo.cs
+++ 03EventConsole/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("02EventConsole")]
+[assembly: AssemblyTitle("03EventConsole")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("02EventConsole")]
+[assembly: AssemblyProduct("03EventConsole")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
