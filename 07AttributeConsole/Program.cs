fa27556d53ded3b2d6c13269fcbbd6895e532a27
张耀文 <1650860607@qq.com>
2021-07-21 17:33:12 +0800

    07AttributeConsole

--- 07AttributeConsole/Program.cs
+++ 07AttributeConsole/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Library;
 
 namespace AttributeConsole
 {
@@ -6,7 +9,25 @@ namespace AttributeConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region 获取全部特性
+            Type type = typeof(MyClass);
+            object[] attributes = type.GetCustomAttributes(true);//获取全部特性
+            if (type.IsDefined(typeof(MyTestAttribute), true))//判断是否存在指定类型的特性
+            {
+                attributes = type.GetCustomAttributes(typeof(MyTestAttribute), true);//获取指定类型的全部特性
+            }
+            #endregion
+
+            #region 获取成员特性
+            PropertyInfo propertyInfo = type.GetProperty("IntProperty");
+            attributes = propertyInfo.GetCustomAttributes(true);//获取属性全部特性
+            if (propertyInfo.IsDefined(typeof(MyTestAttribute), true))//判断是否存在指定类型的特性
+            {
+                attributes = propertyInfo.GetCustomAttributes(typeof(MyTestAttribute), true);//获取指定类型的全部特性
+            }
+            #endregion
+
+            Console.ReadKey();
         }
     }
 }
